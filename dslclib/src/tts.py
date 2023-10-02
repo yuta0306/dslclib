db682524bda859c88c654fe07263353160768dfc
yuta0306 <yuuta.20010306@gmail.com>
2023-10-02 18:31:49 +0900

    monitor queue

--- dslclib/src/tts.py
+++ dslclib/src/tts.py
@@ -1,6 +1,8 @@
 import json
 from typing import Optional
 import time
+import threading
+import warnings
 
 from dslclib.src.base import BaseClient
 
@@ -52,6 +54,24 @@ class SpeechConfig(str):
         return super().__new__(cls, json.dumps(data, ensure_ascii=False))
 
 
+class QueueSupervisor(threading.Thread):
+    def __init__(self, client, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.client = client
+        self.should_stop = threading.Event()
+    
+    def run(self):
+        while not self.should_stop.wait(0):
+            try:
+                self.client.monitor_queue()
+            except ConnectionAbortedError:
+                break
+        print("QueueSupervisorはシャットダウンされました。")
+
+    def kill(self):
+        self.should_stop.set()
+
+
 class Text2SpeechClient(BaseClient):
     """Text2SpeechClient
 
@@ -91,8 +111,27 @@ class Text2SpeechClient(BaseClient):
         >>>
         """
         super().__init__(ip, port)
+        self.queue = list()
+        self.supervisor = QueueSupervisor(self, daemon=False)
+        self.supervisor.start()
+
+    def close(self) -> None:
+        self.supervisor.should_stop.set()
+        return super().close()
+
+    def monitor_queue(self):
+        """
+        キューを監視し、合成音声の再生を終えたのちにキューからコマンドを削除するメソッド
+        **注意**: このメソッドは`self.supervisor`から呼び出されることを推奨する
+        """
+        received = json.loads(str(self.sock.recv(4096).decode()))
+        if "result" in list(received.keys()) and received["result"] == "success-end":
+            self.queue.pop(0)
 
     def is_speaking(self) -> bool:
+        """
+        音声を再生中かどうかを確認するメソッド
+        """
         self.sock.send(json.dumps({"engine": "ISSPEAKING"}).encode())
         received = json.loads(str(self.sock.recv(1024).decode()))
         if "isSpeaking" in list(received.keys()):
@@ -121,6 +160,8 @@ class Text2SpeechClient(BaseClient):
         vocal_tract_length: int = 0,
         duration_information: bool = False,
         speechmark: bool = False,
+        max_num_queue: int = 1,
+        wait_queue: bool = False,
     ) -> None:
         """
         音声にしたい発話テキストと発話音声の設定を入力としてソケットに送信する関数
@@ -143,10 +184,24 @@ class Text2SpeechClient(BaseClient):
         duration_information: bool, default = False
         speechmark: bool, default = False
             スピーチマークが含まれるかどうか
+        max_num_queue: int, default = 1
+            TTSコマンドのキューを最大いくつまでプールできるか
+        wait_queue: bool, default = False
+            max_num_queueのキュー数を超えた場合、キューが空くまで処理を待つかどうか。
+            `wait=False`に設定した場合、TTSのコマンドをキューにスタックせずに終了する。
 
         Returns
         -------
         """
+        if len(self.queue) >= max_num_queue:
+            if wait_queue:
+                warnings.warn("TTSコマンドの最大キュー数を超えたため、キューが空くまで待機します。")
+                while len(self.queue) >= max_num_queue:
+                    time.sleep(0.05)
+            else:
+                warnings.warn(f"TTSコマンドの最大キュー数を超えたため、`{text}`は音声合成されません。", UserWarning)
+                return
+        
         speech = SpeechConfig(
             text=text,
             engine=engine,
@@ -160,6 +215,7 @@ class Text2SpeechClient(BaseClient):
         )
         command = speech + "\n"
         self.sock.send(command.encode())
+        self.queue.append(command)
 
 
 if __name__ == "__main__":
